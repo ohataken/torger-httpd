72143f044efc73f2d94d9366cd13557d118ded1b
Ken Ohata <ohataken@outlook.com>
2016-10-14 18:47:42 +0900

    update torger-httpd.scm

--- torger-httpd.scm
+++ torger-httpd.scm
@@ -2,4 +2,13 @@
 (socket-set-port 8080)
 (bind)
 (listen)
-(recv (accept))
+
+(define sockd -1)
+
+(defun accept-and-receive ()
+  (setq sockd (accept))
+  (println (recv sockd))
+  (send sockd "hello world")
+  (accept-and-receive))
+
+(accept-and-receive)
