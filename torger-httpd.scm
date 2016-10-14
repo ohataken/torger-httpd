c6c6f00e69d3dbae2a1d483741f795a19d73828d
Ken Ohata <ohataken@outlook.com>
2016-10-14 19:07:01 +0900

    update torger-httpds.scm

--- torger-httpd.scm
+++ torger-httpd.scm
@@ -8,7 +8,10 @@
 (defun accept-and-receive ()
   (setq sockd (accept))
   (println (recv sockd))
-  (send sockd "hello world")
+  (send sockd "HTTP/1.1 200 OK\n\r")
+  (send sockd "Content-Length: 11\n\r")
+  (send sockd "\r\n")
+  (send sockd "hello world\r\n")
   (accept-and-receive))
 
 (accept-and-receive)
